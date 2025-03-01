8fa09ffa51dc963141bdf646499cd3f760f18e21
石兴龙 <1003865572@qq.com>
2025-03-01 17:55:32 +0800

    updat

---
+++ docs/.vuepress/components/Welcome-page copy 2.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <iframe class="message-box" src="https://open-chat.xinglong.tech/#/open-chat" />
+</template>
+<style>
+.message-box {
+    width: 60%;
+    height: 400px;
+    background-color: #F5F5F5;
+    margin: 0 auto;
+    margin-left: 20%;
+    padding-top: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+</style>
