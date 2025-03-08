e9ab1d3794649c5dc4da974bf5bc48c10a71a241
石兴龙 <1003865572@qq.com>
2025-03-08 16:25:09 +0800

    publish

--- docs/.vuepress/components/Msg-item.vue
+++ docs/.vuepress/components/Msg-item.vue
@@ -1,24 +1,57 @@
 <script setup>
 import markdown from 'markdown'
-import { defineProps, computed } from 'vue'
+import { defineProps, computed, ref } from 'vue'
 
 const props = defineProps({
     msgData: 'object'
 })
 
+const isCopied = ref(false)
+
 const htmlStr = computed(() => {
-  return markdown.markdown.toHTML(props.msgData.message)
+    return markdown.markdown.toHTML(props.msgData.message)
 })
+
+const handleCopy = () => {
+    navigator.clipboard.writeText(props.msgData.message)
+        .then(() => {
+            isCopied.value = true
+            setTimeout(() => {
+                isCopied.value = false
+            }, 2000)
+        })
+        .catch(err => {
+            console.error('复制失败：', err)
+        })
+}
 </script>
 <template>
-    <div 
-        class="message-item"
-    >  
+    <div class="message-item">
         <div> <img class="message-avatar" src="/images/logo.jpg" alt=""> </div>
-        
+
         <div class="message-content">
             <span class="message-naciname">程序员小石</span>
-            <pre v-html="htmlStr"  ></pre>
+            <pre v-html="htmlStr"></pre>
+            <p style="">
+                <button class="copy-btn" @click="handleCopy">{{ isCopied ? '✓' : '复制' }}</button>
+            </p>
         </div>
     </div>
 </template>
+<style scoped>
+.copy-btn {
+    padding: 4px 12px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    background-color: white;
+    cursor: pointer;
+    transition: all 0.2s;
+    margin-top: 5px;
+    font-size: 10px;
+    color: #333;
+}
+
+.copy-btn:hover {
+    background-color: #f5f5f5;
+}
+</style>
--- docs/.vuepress/components/Welcome-page.vue
+++ docs/.vuepress/components/Welcome-page.vue
@@ -23,73 +23,13 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
 
 <template>
     <div class="message-box">
-
-        <!-- <div 
-            class="message-item"
-            v-for="item of data"
-        >  
-            <div> <img class="message-avatar" src="/images/logo.jpg" alt=""> </div>
-            
-            <div class="message-content">
-                <span class="message-naciname">程序员小石</span>
-                <pre v-html="item.message"  ></pre>
-            </div>
-        </div> -->
-        <MsgItem 
-            v-for="item of data"
-            :msgData="item"
-        />
-        <!-- <div 
-            class="message-item"
-            v-for="item of data"
-        >  
-            <div> <img class="message-avatar" src="/images/logo.jpg" alt=""> </div>
-            
-            <div class="message-content">
-                <span class="message-naciname">程序员小石</span>
-                <pre v-html="item.message"  ></pre>
-            </div>
-        </div> -->
-
-        <!-- <DynamicScroller
-            ref="scroller"
-            :style="isEdit ? {height: `calc(100% - 32px)`} : {height: `100%`}"
-            :items="data"
-            :min-item-size="54"
-            :emit-update="true"
-            class="scroller"
-            keyField="id"
-        >
-            <template #default="{ item, index, active }">
-            <DynamicScrollerItem
-                :item="item"
-                :active="active"
-                :size-dependencies="[
-                    item.message,
-                ]"
-                :data-index="index"
-                :data-active="active"
-            >
-                <div class="message-item">  
-                    <div> <img class="message-avatar" src="/images/logo.jpg" alt=""> </div>
-                    
-                    <div class="message-content">
-                        <span class="message-naciname">程序员小石</span>
-                        <pre v-html="item.message"  ></pre>
-                        <a v-if="isEdit" @click="deleteMessage(item.id)" style="cursor: pointer;" >删除</a>
-                    </div>
-                </div>
-            </DynamicScrollerItem>
-            </template>
-        </DynamicScroller> -->
-
+        <MsgItem v-for="item of data" :msgData="item" />
     </div>
 </template>
 
 <style lang="less">
-
 @media screen and (max-width: 768px) {
-    .message-box{
+    .message-box {
         width: 100% !important;
         height: 100vh;
     }
@@ -98,7 +38,7 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
 @color_you: #95EC69;
 @color_me: #fff;
 
-.message-box{
+.message-box {
     width: 60%;
     height: 400px;
     overflow-y: auto;
@@ -113,7 +53,8 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
     display: flex;
     flex-direction: column-reverse;
 }
-.message-item{
+
+.message-item {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
@@ -121,13 +62,14 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
     padding-bottom: 10px;
 }
 
-.message-item .message-avatar{
+.message-item .message-avatar {
     width: 40px;
     height: 40px;
     user-select: none;
     border-radius: 3px;
 }
-.message-content{
+
+.message-content {
     margin: 0;
     padding: 0;
     margin-left: 10px;
@@ -136,7 +78,8 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
     position: relative;
     align-items: flex-start;
     flex-direction: column;
-    .message-naciname{
+
+    .message-naciname {
         font-size: 10px;
         color: #999;
         padding-bottom: 5px;
@@ -144,17 +87,20 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
         margin: 0;
         user-select: none;
     }
-    p{
-        margin: 0; 
+
+    p {
+        margin: 0;
         padding: 0;
     }
-    img{
+
+    img {
         max-width: 200px !important;
         background-color: @color_you;
         border-radius: 4px;
         border: 1px solid @color_you;
     }
-    pre{
+
+    pre {
         margin: 0;
         padding: 5px;
         background-color: @color_you;
@@ -162,10 +108,12 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
         border-radius: 4px;
         border: 1px solid @color_you;
         white-space: pre-wrap;
-        .message-content pre a{
+
+        .message-content pre a {
             color: #95EC69;
         }
     }
+
     &::before {
         content: '';
         position: absolute;
@@ -174,8 +122,10 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
         z-index: 2;
         border-width: 6px;
         border-style: solid;
-        border-color: transparent @color_you transparent transparent;  /* 假设内容背景色是#f3f3f3 */
+        border-color: transparent @color_you transparent transparent;
+        /* 假设内容背景色是#f3f3f3 */
     }
+
     &::after {
         content: '';
         position: absolute;
@@ -184,25 +134,34 @@ const query = async ({ isToBottom } = { isToBottom: true }) => {
         z-index: 1;
         border-width: 6px;
         border-style: solid;
-        border-color: transparent @color_you transparent transparent;  /* 假设内容背景色是#f3f3f3 */
+        border-color: transparent @color_you transparent transparent;
+        /* 假设内容背景色是#f3f3f3 */
     }
 }
 
 /* .vue-recycle-scroller.direction-vertical:not(.page-mode) */
 
-.message-box :deep(.scroller){
+.message-box :deep(.scroller) {
     /* overflow: hidden !important; */
     overflow-y: overlay !important;
 }
-.message-box :deep(.scroller)::-webkit-scrollbar{
-    width: 10px;  /* 滚动条宽度 */
+
+.message-box :deep(.scroller)::-webkit-scrollbar {
+    width: 10px;
+    /* 滚动条宽度 */
 }
-.message-box :deep(.scroller)::-webkit-scrollbar-thumb{
-    background-color: #c1c1c1;  /* 滑块颜色 */
-    border-radius: 3px;  /* 滑块圆角 */
+
+.message-box :deep(.scroller)::-webkit-scrollbar-thumb {
+    background-color: #c1c1c1;
+    /* 滑块颜色 */
+    border-radius: 3px;
+    /* 滑块圆角 */
 }
-.message-box :deep(.scroller)::-webkit-scrollbar-track{
-    background-color: #f1f1f1;  /* 轨道颜色 */
-    border-radius: 3px;  /* 轨道圆角 */
+
+.message-box :deep(.scroller)::-webkit-scrollbar-track {
+    background-color: #f1f1f1;
+    /* 轨道颜色 */
+    border-radius: 3px;
+    /* 轨道圆角 */
 }
 </style>
